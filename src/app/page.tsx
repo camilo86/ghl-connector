dd91575c28b9a4a82428c7584e437d63973d6d8e
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2024-08-20 22:36:27 -0700

    add welcome card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 export default function Home() {
   return (
-    <div className="container">
-      <h1>GoHighLevel Connector</h1>
+    <div className="h-screen flex justify-center items-center">
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardTitle className="leading-tight">
+            HighLevel Connector for ChatBotBuilder.AI
+          </CardTitle>
+          <CardDescription className="space-y-4">
+            <p>
+              Create HighLevel contacts, notes and more from within your Chat
+              Bot Builder AI flows.
+            </p>
+            <p>
+              To get started, make sure you have access to both your HighLevel
+              and Chat Bot Builder AI account. Once ready, start by logging in
+              to your HighLevel account below.
+            </p>
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <Button className="w-full">Login with HighLevel</Button>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
